2fe0e76379a27ed7e78af956a6fc613a8980b1ca
Daria Mazurash <darya23m@gmail.com>
2023-06-06 18:37:18 +0100

    Added my project

---
+++ utils/database.js
@@ -0,0 +1,12 @@
+const Sequelize = require('sequelize');
+
+const DB_NAME = 'node-todo';
+const USER_NAME = 'root';
+const PASSWORD = '12345678';
+
+const sequelize = new Sequelize(DB_NAME, USER_NAME, PASSWORD, {
+  host: '127.0.0.1',
+  dialect: 'mysql',
+});
+
+module.exports = sequelize
